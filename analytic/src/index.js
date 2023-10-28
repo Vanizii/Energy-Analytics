95cae94eadf8f3ac0f6bd89d5e7663d6f6708157
Sebastian <sebastian.wintgen@informatik.hs-fulda.de>
2023-10-28 19:02:27 +0200

    Sitebar

--- analytic/src/index.js
+++ analytic/src/index.js
@@ -5,6 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Button from '@mui/material/Button';
 
+// Eventuel zu viel
+
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import Toolbar from '@mui/material/Toolbar';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 class Homepage extends React.Component
@@ -15,9 +28,51 @@ class Homepage extends React.Component
     this.state = {};
   }
 
+   drawer = () => {return (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )};
+
   render() {
     return (
       <div>
+         <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: "200px" },
+          }}
+          open
+        >{this.drawer()}</Drawer>
+
+
         <h1>Wilkommen zurück, Marie</h1>
         <h2>Hier sind die wichtigstens Informationen übers Nachhaltigkeitsgymnasium</h2>
         <Button variant="contained">Contained</Button>
