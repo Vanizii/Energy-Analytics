9d3645e17769a90c5aba9e246a17abdb97587eda
Sebastian <sebastian.wintgen@informatik.hs-fulda.de>
2023-10-28 20:06:04 +0200

    Sitebar Design fertiggestellt

--- analytic/src/index.js
+++ analytic/src/index.js
@@ -18,52 +18,70 @@ import ListItemText from '@mui/material/ListItemText';
 import MailIcon from '@mui/icons-material/Mail';
 import Toolbar from '@mui/material/Toolbar';
 
+// Icons
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import SensorDoorIcon from '@mui/icons-material/SensorDoor';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+import ChatIcon from '@mui/icons-material/Chat';
+import LogoutIcon from '@mui/icons-material/Logout';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class Homepage extends React.Component
-{
-  constructor(props)
-  {
+class Homepage extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {};
   }
 
-   drawer = () => {return (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  )};
+  drawer = () => {
+    return (
+      <div>
+        <Toolbar />
+        <Divider />
+        <List>
+          {[
+            { text: 'Dashboard', icon: <DashboardIcon /> },
+            { text: 'Grundriss', icon: <HomeIcon /> },
+            { text: 'Kalender', icon: <CalendarMonthIcon /> },
+            { text: 'Räume', icon: <SensorDoorIcon /> },
+            { text: 'Nutzer', icon: <PersonIcon /> },
+            { text: 'Settings', icon: <SettingsIcon /> },
+          ].map(({ text, icon }, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>{icon}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {[
+            { text: 'Kontakt', icon: <ChatIcon /> },
+            { text: 'Logout', icon: <LogoutIcon /> },
+          ].map(({ text, icon }, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>{icon}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    )
+  };
 
   render() {
     return (
       <div>
-         <Drawer
+
+        <Drawer
           variant="permanent"
           sx={{
             display: { xs: 'none', sm: 'block' },
@@ -72,27 +90,27 @@ class Homepage extends React.Component
           open
         >{this.drawer()}</Drawer>
 
+        <div style={{ marginLeft: '220px' }}>
+          <h1>Wilkommen zurück, Marie</h1>
+          <h2>Hier sind die wichtigstens Informationen übers Nachhaltigkeitsgymnasium</h2>
+          <Button variant="contained">Contained</Button>
 
-        <h1>Wilkommen zurück, Marie</h1>
-        <h2>Hier sind die wichtigstens Informationen übers Nachhaltigkeitsgymnasium</h2>
-        <Button variant="contained">Contained</Button>
+          <div class="uebersicht">
+            <div class="energieverbrauch">2 kWh/m2a</div>
+            <div class="energieersparnis">8 kWh/m2a</div>
+            <div class="Geldersparnis">243</div>
+          </div>
 
-        <div class="uebersicht">
-          <div class="energieverbrauch">2 kWh/m2a</div>
-          <div class="energieersparnis">8 kWh/m2a</div>
-          <div class="Geldersparnis">243</div>
+          <div class="diagramme">
+            <div class="energieverbrauchGrafik">2 kWh/m2a</div>
+            <div class="jahresziele">8 kWh/m2a</div>
+          </div>
+
+
+          <div class="historie">
+            <div>Historie</div>
+          </div>
         </div>
-
-        <div class="diagramme">
-          <div class="energieverbrauchGrafik">2 kWh/m2a</div>
-          <div class="jahresziele">8 kWh/m2a</div>
-        </div>
-
-
-        <div class="historie">
-          <div>Historie</div>
-        </div>
-
       </div>
     );
   }
